f8627df01fca22abf2c8e0564ec297064c6caddf
AnhMTV <maithachvietanh@gmail.com>
2023-03-31 09:48:36 +0700

    Fix "kylinNetwork-NATIVE-KYL" and increase version

---
+++ packages/chain-list/src/migrate/validate-chainAsset.js
@@ -0,0 +1,35 @@
+// Convert chain list
+import fs from 'fs';
+
+const AssetMap = JSON.parse(fs.readFileSync('../data/ChainAsset.json'));
+const ChainMap = JSON.parse(fs.readFileSync('../data/ChainInfo.json'));
+
+Object.entries(AssetMap).forEach(([slug, asset]) => {
+  if (slug !== asset.slug) {
+    console.warn('Not match:', slug, asset.slug);
+  }
+
+  if (!ChainMap[asset.originChain]) {
+    console.warn('Notfound:', slug, asset.originChain, asset.symbol);
+  }
+});
+
+Object.entries(ChainMap).forEach(([slug, chain]) => {
+  if (chain.substrateInfo) {
+    const nativeTokenSlug = `${slug}-NATIVE-${chain.substrateInfo.symbol}`;
+
+    if (!AssetMap[nativeTokenSlug]) {
+      console.warn('Notfound native token:', slug, nativeTokenSlug);
+    }
+  }
+
+  if (chain.evmInfo) {
+    const nativeTokenSlug = `${slug}-NATIVE-${chain.evmInfo.symbol}`;
+
+    if (!AssetMap[nativeTokenSlug]) {
+      console.warn('Notfound native token:', slug, nativeTokenSlug);
+    }
+  }
+});
+
+console.log('Finished');
