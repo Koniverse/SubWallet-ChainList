0ae3ceb3e84ae9d9e1c4989d00d18931ac89f36a
AnhMTV <maithachvietanh@gmail.com>
2023-10-13 16:54:13 +0700

    [Release] beta version 0.2.16-beta.7

--- scripts/strapi/fetch-chains.mjs
+++ scripts/strapi/fetch-chains.mjs
@@ -4,84 +4,24 @@ import {DOWNLOAD_DIR, DOWNLOAD_LINK, downloadFile, graphQLClient, removeDir, wri
 
 const SAVE_PATH = './packages/chain-list/src/data/ChainInfo.json';
 
-
-const query = gql`
-query {
-  chains (pagination: {pageSize: 1000}, sort: "ordinal:asc") {
-    data {
-      attributes {
-        slug
-        name
-        chainStatus
-        providers {
-          name
-          url
-        }
-        isTestnet
-        substrateInfo {
-          paraId
-          relaySlug
-          genesisHash
-          addressPrefix
-          crowdloanUrl
-          chainType
-          blockExplorer
-          symbol
-          existentialDeposit
-          decimals
-          supportStaking
-          hasNativeNft
-          supportSmartContract
-        }
-        evmInfo {
-          evmChainId
-          blockExplorer
-          symbol
-          decimals
-          existentialDeposit
-          supportSmartContract
-          abiExplorer
-        }
-        icon {
-          data {
-            attributes {
-              url
-            }
-          }
-        }
-      }
-    }
-  }
-}
-`;
-
 const main = async () => {
-    const results = await graphQLClient.request(query);
+    const results = await fetch('https://content.subwallet.app/api/list/chain');
+    const data = await results.json();
     const downloadDir = `${DOWNLOAD_DIR}/chains`;
-    const chains = await Promise.all(results.chains.data.map(async chain => {
-        const attributes = chain.attributes;
-        const providers = Object.fromEntries(attributes.providers.map(provider => [provider.name, provider.url]));
-
-        let iconURL = attributes.icon?.data?.attributes?.url;
+    const chains = await Promise.all(data.map(async chain => {
+        let iconURL = chain.icon;
         if (iconURL) {
             try {
-                const newFileName = await downloadFile(iconURL, downloadDir, attributes.slug.toLowerCase());
+                const newFileName = await downloadFile(iconURL, downloadDir, chain.slug.toLowerCase());
                 iconURL = `${DOWNLOAD_LINK}/assets/chains/${newFileName}`;
             } catch (e) {
                 console.error(e);
             }
         }
 
-        return {
-            slug: attributes.slug,
-            name: attributes.name,
-            chainStatus: attributes.chainStatus,
-            providers,
-            isTestnet: attributes.isTestnet,
-            substrateInfo: attributes.substrateInfo,
-            evmInfo: attributes.evmInfo,
-            icon: iconURL,
-        }
+        chain.ordinal&& delete chain.ordinal;
+
+        return chain
     }));
 
     const chainMap = Object.fromEntries(chains.map(chain => [chain.slug, chain]));
