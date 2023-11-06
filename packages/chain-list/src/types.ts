b64da2180df0a2764ad86d5dfd78be8dfb8649e7
AnhMTV <maithachvietanh@gmail.com>
2023-11-06 16:53:10 +0700

    Add extra info to the chain info

--- packages/chain-list/src/types.ts
+++ packages/chain-list/src/types.ts
@@ -45,6 +45,7 @@ export interface _ChainInfo {
   providers: Record<string, string>,
   substrateInfo: _SubstrateInfo | null,
   evmInfo: _EvmInfo | null,
+  extraInfo: _ExtraInfo | null,
   icon: string
 }
 
@@ -74,6 +75,10 @@ export interface _EvmInfo {
 
   supportSmartContract: _AssetType[] | null,
   abiExplorer: string | null
+}
+
+export interface _ExtraInfo {
+  subscanSlug: string | null,
 }
 
 export enum _FundStatus {
