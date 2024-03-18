181f758417caf39c8f65d6330010f3d46e08b875
nampc <namphamc99@gmail.com>
2024-03-18 14:40:13 +0700

    chore: add more common assets and chains

--- packages/chain-list/src/index.ts
+++ packages/chain-list/src/index.ts
@@ -29,7 +29,21 @@ export enum COMMON_CHAIN_SLUGS {
   ASTAR = 'astar',
   WESTEND = 'westend',
   BINANCE = 'binance',
-  ASTAR_EVM = 'astarEvm'
+  ASTAR_EVM = 'astarEvm',
+  HYDRADX = 'hydradx_main',
+  ETHEREUM_SEPOLIA = 'sepolia_ethereum',
+  CHAINFLIP_POLKADOT = 'chainflip_dot',
+}
+
+export enum COMMON_ASSETS {
+  DOT = 'polkadot-NATIVE-DOT',
+  ETH = 'ethereum-NATIVE-ETH',
+  KSM = 'kusama-NATIVE-KSM',
+  ETH_SEPOLIA = 'sepolia_ethereum-NATIVE-ETH',
+  PDOT = 'chainflip_dot-NATIVE-pDOT',
+  HDX = 'hydradx_main-NATIVE-HDX',
+  USDC_ETHEREUM = 'ethereum-ERC20-USDC-0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+  USDC_SEPOLIA = 'sepolia_ethereum-ERC20-USDC-0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238',
 }
 
 export const _DEFAULT_CHAINS = [
