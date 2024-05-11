0c044403917b204d0b0e025dcf97a2e231812c8d
bluezdot <72647326+bluezdot@users.noreply.github.com>
2024-05-11 11:31:54 +0700

    Integrate Rune asset

--- packages/chain-list/src/types.ts
+++ packages/chain-list/src/types.ts
@@ -29,7 +29,8 @@ export enum _AssetType {
   ERC721 = 'ERC721',
   PSP22 = 'PSP22',
   PSP34 = 'PSP34',
-  UNKNOWN = 'UNKNOWN'
+  UNKNOWN = 'UNKNOWN',
+  RUNE = 'RUNE'
 }
 
 export enum _SubstrateChainType {
