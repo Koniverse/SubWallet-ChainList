5b9cf5ae40945fa416c2f51022db2f6fbb3f4335
bluezdot <72647326+bluezdot@users.noreply.github.com>
2024-08-02 14:19:32 +0700

    Update ton interface

--- packages/chain-list/src/types.ts
+++ packages/chain-list/src/types.ts
@@ -31,6 +31,7 @@ export enum _AssetType {
   PSP34 = 'PSP34',
   GRC20 = 'GRC20',
   VFT = 'VFT',
+  TEP74 = 'TEP74',
   GRC721 = 'GRC721',
   RUNE = 'RUNE',
   BRC20 = 'BRC20',
@@ -52,6 +53,7 @@ export interface _ChainInfo {
   evmInfo: _EvmInfo | null,
   bitcoinInfo: _BitcoinInfo | null;
   extraInfo: _ExtraInfo | null,
+  tonInfo: _TonInfo | null
   icon: string
 }
 
@@ -105,6 +107,13 @@ export interface _BitcoinInfo {
   decimals: number;
   symbol: string;
   bitcoinNetwork: BitcoinNetworkMode
+}
+
+export interface _TonInfo {
+  blockExplorer: string | null;
+  existentialDeposit: string;
+  decimals: number;
+  symbol: string;
 }
 
 export interface _ExtraInfo {
