6d9ab1e4050fb25f56643718ef9a4b43e2a22623
bluezdot <72647326+bluezdot@users.noreply.github.com>
2025-06-17 11:15:13 +0700

    add script get legacy chain

---
+++ scripts/exports/export-legacy-chains.js
@@ -0,0 +1,24 @@
+import ChainInfoMap from '../../packages/chain-list/src/data/ChainInfo.json' assert {type: 'json'};
+import fs from "fs";
+
+const chainRpcMap = {}
+
+Object.entries(ChainInfoMap).forEach(([chainSlug, chainInfo]) => {
+  if (!!chainInfo.substrateInfo && chainInfo.chainStatus === 'ACTIVE') {
+    chainRpcMap[chainSlug] = Object.values(chainInfo.providers);
+  }
+});
+
+console.log(`List legacy chains: ${chainRpcMap}`);
+
+try {
+  const fileName = `exports/chain-rpcs.json`;
+
+  fs.mkdirSync('exports', { recursive: true });
+  fs.writeFile(fileName, JSON.stringify(chainRpcMap), function(err) {
+    if (err) throw err;
+    console.log(`Saved ${fileName} successfully`);
+  });
+} catch (err) {
+  console.error(err);
+}
