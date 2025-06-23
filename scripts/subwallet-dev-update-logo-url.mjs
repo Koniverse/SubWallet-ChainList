6dc9195f86511b5369012d5a7c98ab8da16bdafd
AnhMTV <maithachvietanh@gmail.com>
2025-06-23 12:27:56 +0700

    Update CI/CD for chain-list (2)

--- scripts/subwallet-dev-update-logo-url.mjs
+++ scripts/subwallet-dev-update-logo-url.mjs
@@ -4,6 +4,7 @@ const BRANCH_NAME = process.env.BRANCH_NAME || 'dev';
 const CUSTOM_LOGO_PREFIX = process.env.CUSTOM_LOGO_PREFIX;
 const DEV_LOGO_PREFIX = CUSTOM_LOGO_PREFIX || "https://dev.sw-chain-list-assets.pages.dev";
 const PRODUCT_LOGO_PREFIX = "https://chain-list-assets.subwallet.app";
+const SRC_DIR = './packages/chain-list/src/data/';
 const BUILD_DIR = './packages/chain-list/build/data/';
 const BUILD_CJS_DIR = './packages/chain-list/build/cjs/data/';
 const dataFiles = [
@@ -23,16 +24,18 @@ const main = async () => {
 
   // Override logo prefix in data files
   for (const file of dataFiles) {
+    const srcPath = `${SRC_DIR}${file}`;
     const filePath = `${BUILD_DIR}${file}`;
-    const migrationData = fs.readFileSync(filePath, 'utf-8').replaceAll('"icon": "/', `"icon": "${logoUrlPrefix}/` );
+    const migrationData = fs.readFileSync(srcPath, 'utf-8').replaceAll('"icon": "/', `"icon": "${logoUrlPrefix}/` );
 
     fs.writeFileSync(filePath, migrationData);
   }
 
   // Override logo prefix in logo map files
   for (const file of mapFiles) {
+    const srcPath = `${SRC_DIR}${file}`;
     const filePath = `${BUILD_DIR}${file}`;
-    const migrationData = fs.readFileSync(filePath, 'utf-8').replaceAll('": "/', `": "${logoUrlPrefix}/` );
+    const migrationData = fs.readFileSync(srcPath, 'utf-8').replaceAll('": "/', `": "${logoUrlPrefix}/` );
 
     fs.writeFileSync(filePath, migrationData);
   }
