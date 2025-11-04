67d6962c4c3133f001891ae9f09d33046b7cfd2f
AnhMTV <maithachvietanh@gmail.com>
2025-11-04 16:45:52 +0700

    [Patch] [Beta] Version 0.2.121-beta.3 (Chery-pick from issue-4812-extension)

--- scripts/patch/patch-api.mjs
+++ scripts/patch/patch-api.mjs
@@ -2,8 +2,8 @@ import {Md5} from "ts-md5";
 
 export const DEV_LOGO_PREFIX = "https://dev.sw-chain-list-assets.pages.dev";
 export const PRODUCT_LOGO_PREFIX = "https://chain-list-assets.subwallet.app";
-export const STABLE_VERSION = '0.2.118';
-export const PATCH_VERSION = '0.2.119-beta.1';
+export const STABLE_VERSION = '0.2.120';
+export const PATCH_VERSION = '0.2.121-beta.3';
 export const PATCH_SAVE_DIR =  `./packages/chain-list-assets/public/patch/${STABLE_VERSION}`;
 export const PATCH_SAVE_DEV = `${PATCH_SAVE_DIR}/preview.json`;
 export const PATCH_SAVE_STABLE = `${PATCH_SAVE_DIR}/list.json`;
