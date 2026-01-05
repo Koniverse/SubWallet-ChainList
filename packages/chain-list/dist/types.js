edf0550b7313c5086336d7a3cb17e47819662596
AnhMTV <maithachvietanh@gmail.com>
2026-01-05 16:43:30 +0700

    Remove dist and update .gitignore

--- packages/chain-list/dist/types.js
+++
@@ -1,9 +0,0 @@
-var E = /* @__PURE__ */ ((N) => (N.ACTIVE = "ACTIVE", N.INACTIVE = "INACTIVE", N.STOPPED = "STOPPED", N))(E || {}), I = /* @__PURE__ */ ((N) => (N.ONLINE = "ONLINE", N.OFFLINE = "OFFLINE", N.UNSTABLE = "UNSTABLE", N))(I || {}), C = /* @__PURE__ */ ((N) => (N.NATIVE = "NATIVE", N.LOCAL = "LOCAL", N.ERC20 = "ERC20", N.ERC721 = "ERC721", N.PSP22 = "PSP22", N.PSP34 = "PSP34", N.GRC20 = "GRC20", N.VFT = "VFT", N.GRC721 = "GRC721", N.RUNE = "RUNE", N.BRC20 = "BRC20", N.TEP74 = "TEP74", N.CIP26 = "CIP26", N.UNKNOWN = "UNKNOWN", N))(C || {}), P = /* @__PURE__ */ ((N) => (N.RELAYCHAIN = "RELAYCHAIN", N.PARACHAIN = "PARACHAIN", N))(P || {}), r = /* @__PURE__ */ ((N) => (N.IN_AUCTION = "in_auction", N.WON = "won", N.WITHDRAW = "withdraw", N.FAILED = "failed", N))(r || {}), R = /* @__PURE__ */ ((N) => (N.XCM = "XCM", N.MANTA_ZK = "MANTA_ZK", N.SWAP = "SWAP", N))(R || {});
-export {
-  R as _AssetRefPath,
-  C as _AssetType,
-  I as _ChainProviderStatus,
-  E as _ChainStatus,
-  r as _FundStatus,
-  P as _SubstrateChainType
-};
